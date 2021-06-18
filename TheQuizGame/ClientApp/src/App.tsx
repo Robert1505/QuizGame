53c0966af4abf7d1ce6eaf60ab7395c2e17d4dba
Patainea Robert <patainearobert@yahoo.com>
2021-06-18 18:19:43 +0300

    deleted unused files

--- TheQuizGame/ClientApp/src/App.tsx
+++ TheQuizGame/ClientApp/src/App.tsx
@@ -1,13 +1,12 @@
 import * as React from "react";
 import { Route } from "react-router";
-import Layout from "./components/Layout";
 import "./custom.css";
 import LandingPage from "./Pages/LandingPage";
 
 export default () => (
-  <Layout>
+  <React.Fragment>
     <Route exact path="/">
       <LandingPage />
     </Route>
-  </Layout>
+  </React.Fragment>
 );
